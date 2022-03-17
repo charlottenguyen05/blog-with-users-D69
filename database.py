f83fd5cbfa60a739e5c48c69fa57bdc847b65780
Linh Nhi <nhinguyenngoclinh2005@gmail.com>
2022-03-18 01:22:21 +0700

    Initial Commit

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("KEY")
 # Update the app config to use "DATABASE_URL" environment variable if provided,
 # but if it's None (e.g. when running locally) then we can provide sqlite:///blog.db as the alternative.
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL_1",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Base = declarative_base()
